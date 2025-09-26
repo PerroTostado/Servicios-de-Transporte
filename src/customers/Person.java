5eaba68e075e05140fe8a7c002048ff09a0f9fe4
CENTIC <CENTIC@10.6.65.79>
2025-09-26 11:36:14 -0500

    XDD

---
+++ src/customers/Person.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customers;
+
+
+public class Person {
+    public class customers {
+    private String name;
+    private int age;
+public customers(){
+    
+}
+public customers(String name, int age){
+    this.name=name;
+    this.age=age;
+}
+protected final void setName(String name){
+    this.name=name;
+}
+protected final String setName(){
+    return this.name;
+}
+protected final void setAge(int age){
+    this.age=age;
+}
+protected final int setAge(){
+    return this.age;
+}
+@Override
+public String toString(){
+    String XD= "";
+    return XD;
+}
+}
+}
